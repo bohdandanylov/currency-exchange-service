ead73ac2f47a640d63a70aa2c304033cfba9a036
Bohdan Danylov <bohdandanylov@gmail.com>
2024-01-13 14:06:08 -0800

    v1.4 - Configure JPA and Initialized Data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10001, 'USD', 'CAD', 1.34, '');
+
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10002, 'USD', 'EUR', 0.91, '');
+
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10003, 'USD', 'GBP', 0.78, '');
